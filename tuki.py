ee13a7e38709d7af680a6b3ef7fe01be2ebd4c54
Ralf Haring <ralf.haring@gmail.com>
2014-04-07 02:34:09 -0400

    small cleanup of tuki/rasl/bone script

--- tuki.py
+++ tuki.py
@@ -3,56 +3,62 @@ import requests
 import os
 import argparse
 
-series = {'t' : 'tuki', 'b' : 'bone', 'r' : 'rasl'}
-cover = {'t' : 'http://www.boneville.com/comic/tuki-season-one-cover',
-         'b' : 'http://www.boneville.com/comic/bone-issue-one-cover-2',
-         'r' : 'http://www.boneville.com/comic/rasl-issue-one-cover'}
-pages = set()
-
 # find and store the image on the current page
 # then drill down to the next page if there is one
-def findPages(url, page, series, pages):
+def find_pages(url, page, series, pages):
     r = requests.get(url)
-#    print r, r.ok
     if r.ok:
         soup = BeautifulSoup(r.content)
         div = soup.find(id='comic')
         image = div.img['src']
-        pages.add((image, series + '-' + str(page)))
+        pages.add((image, series, str(page)))
         # if the image is a link, keep going
         # otherwise this is last page and bump out
         if div.a is not None:
-            findPages(div.a['href'], page + 1, series, pages)
+            find_pages(div.a['href'], page + 1, series, pages)
     else:
-        findPages(url, page, series, pages)
+        print 'error1', r, url
+        find_pages(url, page, series, pages)
 
-# try to get a random image from those previously stored
-# and keep retrying until all are complete
-def downloadPages(pages):
+# create dir path if it doesn't exist
+def check_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+# pull out random pages and attempt to save them locally.
+# if that fails, put them back in the pool and try again.
+def download_pages(pages, image_dir):
     while pages:
-        image, name = pages.pop()
+        image, series, page = pages.pop()
         ext = image.rsplit('.')[-1]
-        # rename the file so they're easily sorted
-        new_name = name + '.' + ext.lower()
-        local_file = os.getcwd() + '\\' + new_name
+        # rename the files so they're easily sorted
+        new_name = series + '-' + page + '.' + ext.lower()
+        local_file = image_dir + '\\' + series + '\\' + new_name
         r = requests.get(image)
-#        print r, r.ok
         if r.ok:
             with open(local_file, 'wb') as f:
                 print 'downloading', image.rsplit('/')[-1] + ' -> ' + new_name
                 f.write(r.content)
         else:
-            pages.add((image, name))
+            print 'error2', r, image
+            pages.add((image, series, page))
 
 def main(**kwargs):
-    for code, name in series.iteritems():
-        if kwargs[code]:
-            print 'starting to find pages for', name
-            findPages(cover[code], 1, name, pages)
-#            print len(pages)
-    downloadPages(pages)
+    series = [('t', 'tuki', 'http://www.boneville.com/comic/tuki-season-one-cover'),
+              ('b', 'bone', 'http://www.boneville.com/comic/bone-issue-one-cover-2'),
+              ('r', 'rasl', 'http://www.boneville.com/comic/rasl-issue-one-cover')]
+    pages = set()
+    image_dir = os.getcwd()
+
+    for flag, name, cover in series:
+        if kwargs[flag]:
+            print 'finding pages for', name
+            find_pages(cover, 1, name, pages)
+            check_dir(image_dir + '\\' + name)
+    download_pages(pages, image_dir)
 
 if __name__ == '__main__':
+    # call with -t, -b, -r, or any combination like -rt or -tbr
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', action='store_true', help='download Tuki')
     parser.add_argument('-b', action='store_true', help='download Bone')
@@ -60,6 +66,7 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
 
     if not any(args.values()):
+        # must choose at least one
         parser.print_help()
     else:
         main(**args)
