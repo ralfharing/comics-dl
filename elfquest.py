eb37eb980f4a1c2865c73380e7d09d509c50658e
Ralf Haring <ralf.haring@gmail.com>
2014-04-09 14:42:38 -0400

    update elfquest script a whole lot

--- elfquest.py
+++ elfquest.py
@@ -2,48 +2,111 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-base = 'http://www.elfquest.com/gallery/OnlineComics/'
-resp = requests.get(base)
-soup = BeautifulSoup(resp.content)
-#yes = ['ASH/', 'TS/']
+lookup = {
+'OQ/' : '1. The Original Quest',
+'SABM/' : '2. Siege at Blue Mountain',
+'KOBW/' : '3. Kings of the Broken Wheel',
+'HY/' : '4. Hidden Years',
+'SH/' : '5. Shards',
+'NB/' : '6. New Blood',
+'TC/' : '7. Blood of Ten Chiefs',
+'KA/' : '8. Kahvi',
+'TS/' : '9. Two-Spear',
+'JK/' : '10. Jink',
+'RB/' : '11. The Rebels',
+'WD/' : '12. WaveDancers',
+'MET/' : '13. Metamorphosis',
+'EQ2/' : '14. ElfQuest volume 2 (by issue)',
+'DT/' : '14. ElfQuest volume 2 (by story)/Dreamtime',
+'DTC/' : '14. ElfQuest volume 2 (by story)/Dreamtime (color)',
+'WH/' : '14. ElfQuest volume 2 (by story)/Wild Hunt',
+'FE/' : '14. ElfQuest volume 2 (by story)/Fire-Eye',
+'WDa/' : '14. ElfQuest volume 2 (by story)/WaveDancers',
+'RC/' : '14. ElfQuest volume 2 (by story)/Rogue\'s Curse',
+'FQ/' : '14. ElfQuest volume 2 (by story)/FutureQuest',
+'WR/' : '14. ElfQuest volume 2 (by story)/Wolfrider!',
+'MEN3/' : '14. ElfQuest volume 2 (by story)/Mender\'s Tale part 3',
+'MEN4/' : '14. ElfQuest volume 2 (by story)/Mender\'s Tale part 4',
+'MEN5/' : '14. ElfQuest volume 2 (by story)/Mender\'s Tale part 5',
+'BS/' : '15. one shots/Bedtime Stories',
+'FFJ/' : '15. one shots/The Jury',
+'FFR/' : '15. one shots/Rogue\'s Curse',
+'WP/' : '15. one shots/Worldpool',
+'KC/' : '15. one shots/King\'s Cross',
+'HS/' : '15. one shots/Homespun',
+'WGA/' : '15. one shots/Courage, By Any Other Name',
+'SS2WS/' : '16. Wolfshadow',
+'SS2REC/' : '17. Recognition/Recognition (part 1)',
+'SS2aREC/' : '17. Recognition/Recognition (parts 1 & 2)',
+'IABB/' : '18. In All But Blood',
+'SAS/' : '19. The Searcher and the Sword',
+'DISC/' : '20. The Discovery',
+'EQFQ/' : '21. Final Quest prologue',
+'ESS/' : 'misc/Essential ElfQuest',
+'WDX/' : 'misc/WaveDancers (lost chapters)',
+'HYC/' : 'misc/Hidden Years (color)',
+'KAC/' : 'misc/Kahvi (color)',
+'SHC/' : 'misc/Shards (color)',
+'ASH/' : 'misc/Hidden Years & Shards ashcan',
+'GOHO/' : 'misc/A Gift of Her Own',
+'PressWR94/' : 'misc/movie storyboards 1994',
+'PressWR95/' : 'misc/movie storyboards 1995',
+'XAN/' : 'misc/Xanth - Return to Centaur'
+}
 
-ignore = ['Parent Directory']
+# exclude previous and thumbnail directories as well as misc other files
 def is_valid(entry):
-    return entry not in ignore \
-           and 'thumb' not in entry \
-           and 'html' not in entry \
-           and 'gif' not in entry \
-           and 'tif' not in entry
+    ignore = ('Parent Directory', 'html', 'gif', 'tif', 'php', 'thumb/')
+    return not entry.endswith(ignore)
 
-links = set(x['href'] for x in soup('a') if is_valid(list(x.stripped_strings)[0]))
-files = set(l for l in links if not l.endswith('/'))
-dirs = links - files
-
-image_dir = os.getcwd() + '\\images\\'
+# create dir path if it doesn't exist
 def check_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def download_files(local_dir, base_url, files):
-    check_dir(local_dir)
-    for file in files:
-        r = requests.get(base_url + file)
-        with open(local_dir + file.lower(), 'wb') as f:
-            f.write(r.content)
+# each member of the returned set 'pages' is a tuple in
+# the format (image url, full local file path)
+def find_pages(local_dir, base_url, pages, is_root):
+    r = requests.get(base_url)
+    if r.ok:
+        soup = BeautifulSoup(r.content)
+        # filter out invalid entries in current dir
+        links = set(x['href'] for x in soup('a') if is_valid(''.join(x.stripped_strings)))
+        # separate into files and dirs
+        files = set(l for l in links if not l.endswith('/'))
+        dirs = links - files
+        for f in files:
+            pages.add((base_url + f, local_dir + f.lower()))
+        for d in dirs:
+            if is_root: print 'finding pages for', lookup[d].split('. ')[-1]
+            # rename the first level dirs so they're easily sorted
+            sub_dir = local_dir + lookup[d] + '/' if is_root else local_dir + d
+            check_dir(sub_dir)
+            find_pages(sub_dir, base_url + d, pages, False)
+    else:
+        print 'error1', r, base_url
 
-#def download(local_dir, base_url, files, dirs, skip_check=False):
-def download(local_dir, base_url, files, dirs):
-    download_files(local_dir, base_url, files)
-    for dir in dirs:
-#        if dir in yes or skip_check:
-        sub_local_dir = local_dir + dir.replace('/', '\\')
-        check_dir(sub_local_dir)
-        resp = requests.get(base_url + dir)
-        soup = BeautifulSoup(resp.content)
-        links = set(x['href'] for x in soup('a') if is_valid(list(x.stripped_strings)[0]))
-        sub_files = set(l for l in links if not l.endswith('/'))
-        sub_dirs = links - sub_files
-#        download(sub_local_dir, base_url + dir, sub_files, sub_dirs, True)
-        download(sub_local_dir, base_url + dir, sub_files, sub_dirs)
+# pull out random pages and attempt to save them locally.
+# if that fails, put them back in the pool and try again.
+def download_pages(pages):
+    while pages:
+        image, local_file = pages.pop()
+        new_name = local_file.split('/')[-1]
+        r = requests.get(image)
+        if r.ok:
+            with open(local_file, 'wb') as f:
+                print 'downloading', image.split('/')[-1] + ' -> ' + new_name
+                f.write(r.content)
+        else:
+            print 'error2', r, image
+            pages.add((image, local_file))
 
-download(image_dir, base, files, dirs)
+def main():
+    base_url = 'http://www.elfquest.com/gallery/OnlineComics/'
+    image_dir = os.getcwd() + '/elfquest/'
+    pages = set()
+    find_pages(image_dir, base_url, pages, True)
+    download_pages(pages)
+
+if __name__ == '__main__':
+    main()
